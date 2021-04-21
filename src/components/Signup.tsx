fc6e0ff97638d90173a2aae9f80f608a4ecfdd9f
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-04-21 15:25:40 +0700

    add signup form

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,3 +1,90 @@
-import React from 'react';
+import { useFormik } from 'formik';
+import React, { useEffect, useRef } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-export const Signup: React.FC = () => <div>signup</div>;
+import { getSignupSchema } from '../utils/validators';
+
+export const Signup: React.FC = () => {
+    const nameRef: React.MutableRefObject<HTMLInputElement> = useRef();
+    const f = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+            confirmPassword: '',
+        },
+        validationSchema: getSignupSchema(),
+        onSubmit: () => null,
+    });
+
+    useEffect(() => {
+        nameRef.current.focus();
+    }, []);
+
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-center pt-5">
+                <div className="col-sm-4">
+                    <Form className="p-3" onSubmit={f.handleSubmit}>
+                        <Form.Group>
+                            <Form.Label htmlFor="username">Имя пользователя</Form.Label>
+                            <Form.Control
+                                id="username"
+                                name="username"
+                                autoComplete="username"
+                                value={f.values.username}
+                                onChange={f.handleChange}
+                                onBlur={f.handleBlur}
+                                isInvalid={f.errors.username && !!f.errors.username}
+                                required={true}
+                                placeholder="От 3 до 20 символов"
+                                ref={nameRef}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {f.errors.username && f.errors.username}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="password">Пароль</Form.Label>
+                            <Form.Control
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="new-password"
+                                value={f.values.password}
+                                onChange={f.handleChange}
+                                onBlur={f.handleBlur}
+                                isInvalid={f.touched.password && !!f.errors.password}
+                                required={true}
+                                placeholder="Не менее 6 символов"
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {f.touched.password && f.errors.password}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="confirmPassword">Подтвердите пароль</Form.Label>
+                            <Form.Control
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                type="password"
+                                autoComplete="new-password"
+                                value={f.values.confirmPassword}
+                                onChange={f.handleChange}
+                                onBlur={f.handleBlur}
+                                isInvalid={f.touched.confirmPassword && !!f.errors.confirmPassword}
+                                required={true}
+                                placeholder="Пароли должны совпадать"
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {f.touched.confirmPassword && f.errors.confirmPassword}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Button type="submit" variant="outline-primary" className="w-100 mb-3">
+                            Зарегистрироваться
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ src/utils/validators.ts
@@ -0,0 +1,32 @@
+import * as yup from 'yup';
+
+interface User {
+    username: string;
+    password: string;
+    confirmPassword?: string;
+}
+
+export const getLoginSchema = (): yup.SchemaOf<User> => {
+    const schema = yup.object({
+        username: yup.string().min(3), // TODO make validation
+        password: yup.string().min(3), // TODO make validation
+        confirmPassword: yup.string().strip(),
+    });
+
+    return schema;
+};
+
+export const getSignupSchema = (): yup.SchemaOf<User> => {
+    const schema = yup.object({
+        username: yup
+            .string()
+            .trim()
+            .min(3, 'От 3 до 20 символов')
+            .max(20, 'От 3 до 20 символов')
+            .required('Обязательное поле'),
+        password: yup.string().trim().min(6, 'Не менее 6 символов').required('Обязательное поле'),
+        confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Пароли должны совпадать'),
+    });
+
+    return schema;
+};

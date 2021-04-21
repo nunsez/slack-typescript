ccf60f534eeea4ff6b39d3aba63af5f17dd2ca5f
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-04-21 15:25:53 +0700

    update login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import { useFormik } from 'formik';
+import React, { useRef, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
+import { getLoginSchema } from '../utils/validators';
+
 export const Login: React.FC = () => {
+    const nameRef: React.MutableRefObject<HTMLInputElement> = useRef();
+    const f = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        validationSchema: getLoginSchema(),
+        onSubmit: () => null,
+    });
+
+    useEffect(() => {
+        nameRef.current.focus();
+    }, []);
+
     return (
         <div className="container-fluid">
             <div className="row justify-content-center pt-5">
                 <div className="col-sm-4">
-                    <Form className="p-3">
+                    <Form className="p-3" onSubmit={f.handleSubmit}>
                         <Form.Group>
                             <Form.Label htmlFor="username">Ваш ник</Form.Label>
                             <Form.Control
                                 id="username"
                                 name="username"
                                 autoComplete="username"
-                                value=""
+                                value={f.values.username}
+                                onChange={f.handleChange}
+                                onBlur={f.handleBlur}
+                                isInvalid={!!f.errors.username}
                                 required={true}
+                                ref={nameRef}
                             />
                         </Form.Group>
                         <Form.Group>
@@ -24,11 +45,14 @@ export const Login: React.FC = () => {
                                 name="password"
                                 type="password"
                                 autoComplete="current-password"
-                                value=""
+                                value={f.values.password}
+                                onChange={f.handleChange}
+                                onBlur={f.handleBlur}
+                                isInvalid={!!f.errors.password}
                                 required={true}
                             />
                             <Form.Control.Feedback type="invalid">
-                                Неверные имя пользователя или пароль
+                                {(f.errors.username || f.errors.password) && 'Неверные имя пользователя или пароль'}
                             </Form.Control.Feedback>
                         </Form.Group>
                         <Button type="submit" variant="outline-primary" className="w-100 mb-3">
